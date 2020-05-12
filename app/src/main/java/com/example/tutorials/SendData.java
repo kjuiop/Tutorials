da2c7c2ea726657366d4a0c08c1465f6fecb5452
kjuiop <copying_y@naver.com>
2020-05-12 21:39:57 +0900

    send data complete

--- app/src/main/java/com/example/tutorials/SendData.java
+++ app/src/main/java/com/example/tutorials/SendData.java
@@ -1,7 +1,13 @@
 package com.example.tutorials;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SendData extends AppCompatActivity {
 
@@ -9,5 +15,23 @@ public class SendData extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_data);
+
+        final EditText nameText = (EditText) findViewById(R.id.nameText);
+        final Button nameButton = (Button) findViewById(R.id.nameButton);
+
+        nameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameText.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                intent.putExtra("nameText", name);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void onBackButtonClick(View v) {
+        Toast.makeText(getApplicationContext(), "돌아가기 버튼을 눌러써요.", Toast.LENGTH_LONG).show();
+        finish();
     }
 }
